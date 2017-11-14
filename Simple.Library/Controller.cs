7dbcf0d0cbf6bef4630d496a2d2ef7c14da4ea04
ElStefan <das_gigabike@hotmail.com>
2017-11-14 10:18:46 +0100

    Add project files.

---
+++ Simple.Library/Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Simple.Library
+{
+    /// <summary>
+    /// Controller for library
+    /// </summary>
+    public class Controller
+    {
+        private bool _stop;
+
+        /// <summary>
+        /// Start thread
+        /// </summary>
+        public void Start()
+        {
+            if (Assembly.GetAssembly(typeof(Controller)).GetName().Version > new Version(1, 0, 0, 0))
+            {
+                // new version contains really bad code, it crashes immediately at start
+                throw new InitializationException();
+            }
+
+            new Thread(KeepAliveThread).Start();
+        }
+
+        private void KeepAliveThread()
+        {
+            while (!_stop)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        /// <summary>
+        /// Stop thread
+        /// </summary>
+        public void Stop()
+        {
+            this._stop = true;
+        }
+    }
+}
---
+++ Simple.Service/WindowsService.cs
@@ -0,0 +1,50 @@
+﻿using Simple.Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Service
+{
+    public partial class WindowsService : ServiceBase
+    {
+        private Controller _controller = new Controller();
+
+        public WindowsService()
+        {
+            InitializeComponent();
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                if (EventLog.SourceExists(ServiceName))
+                {
+                    EventLog.WriteEntry(ServiceName,
+                        "Fatal Exception : " + Environment.NewLine +
+                        e.ExceptionObject, EventLogEntryType.Error);
+                }
+            };
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                _controller.Start();
+            }
+            catch
+            {
+                ExitCode = 1064;
+                throw;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            _controller.Stop();
+        }
+    }
+}
